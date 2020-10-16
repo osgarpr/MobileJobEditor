23c095aa5a83535fd4509ec77794c6b51e52a86a
Osvaldo Garcia <osgar@icloud.com>
2020-10-16 06:55:55 -0400

    Added Detail Customer VC, trying to present results on the View Controller

---
+++ MobileJobEditor/CustomerDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CustomerDetailViewController.swift
+//  MobileJobEditor
+//
+//  Created by Osvaldo Garcia on 10/16/20.
+//
+
+import UIKit
+
+
+class CustomerDetailViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MobileJobEditor/ViewController.swift
+++ MobileJobEditor/ViewController.swift
@@ -9,16 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var CustomerTableView: UITableView!
+    
+    
     var model = CustomerModel()
+    var customer = [Customers]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Set the viewcontroller as the delegate and datasource of the CustomerTableView
+        CustomerTableView.delegate = self
+        CustomerTableView.dataSource = self
         
         //Get the Customers from the Customer Model
         model.delegate = self
         model.getCustomers()
     }
 }
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //customer is traking an array of Customers, for the number of rows we will get the count
+        return customer.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //Get a Cell
+        let cell = CustomerTableView.dequeueReusableCell(withIdentifier: "CustomerCell", for: indexPath)
+        
+        //TODO: Customize the cell
+        
+        //Return the cell
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
+
+
+
 
 extension ViewController: CustomerModelProtocol {
     
@@ -26,6 +62,14 @@ extension ViewController: CustomerModelProtocol {
     
     func customersRetrieved(_ customer: [Customers]) {
         print("Customer Return from Model")
+        
+        //Set the customers property of the viewcontroller to the customers passed back from model
+        self.customer = customer
+        
+        //Refresh the Customer table view
+        CustomerTableView.reloadData()
+        
+
     }
 
 }
